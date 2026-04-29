f95a771a8124356f15547de9a2ff1dc1264957d3
A <146786899+anushkaramwani07@users.noreply.github.com>
2026-04-29 21:52:00 +0000

    added more code

--- scripts.js
+++ scripts.js
@@ -1,16 +1,21 @@
 
 
 
-const genres = ["Action", "Comedy", "Horror","crime"];
+const genres = ["Action", "Comedy", "Horror", "crime"];
 
 // finds the "spinButton" element
 const button = document.getElementById("spinButton");
 const result = document.getElementById("result");
 
-button.addEventListener("click", function() {
-    const randomIndex = Math.floor(Math.random() * genres.length);
-   const selectedGenre = genres[randomIndex];
-   
-   //prints the result to webpage
-    result.textContent = "You Got: " + selectedGenre;
+//it should not crash on pages with no spin the wheel feature:
+if (button) {
+
+    button.addEventListener("click", function () {
+        const randomIndex = Math.floor(Math.random() * genres.length);
+        const selectedGenre = genres[randomIndex];
+
+        //prints the result to webpage
+        result.textContent = "You Got: " + selectedGenre;
+    
 });
+}
