851aa30a41ae8ec2f29c6b96c2c70b413b03eeef
A <146786899+anushkaramwani07@users.noreply.github.com>
2026-05-06 00:57:34 +0000

    idk

--- scripts.js
+++ scripts.js
@@ -12,11 +12,11 @@ const resultsDiv = document.getElementById("results");
 const apiKey = "ce9abacc48c7d1abc05b7ee6f534452a";
 
 //console shows whatever user picked.
-genreSelect.addEventListener("change", function(){
-    const selectGenre = this.value;
+genreSelect.addEventListener("change", function () {
+  const selectGenre = this.value;
 
-    console.log("selected genre:", selectGenre);
-    fetchGenre(selectGenre);
+  console.log("selected genre:", selectGenre);
+  fetchGenre(selectGenre);
 });
 
 const genreMap = {
@@ -50,57 +50,57 @@ const genres = [
 
 
 const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MjFiNzYwNjlmNjRiNjU5MmY4NGJiOTQ5YmQ5ZGVkNiIsIm5iZiI6MTc3NzkxNTYwMS41MzIsInN1YiI6IjY5ZjhkNmQxY2ZlMTA5OTNlYmRmNTczYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BOdM0xjg3W246TZQTxQtxeTRmgXZD73klu19tQkiWF8'
-    }
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MjFiNzYwNjlmNjRiNjU5MmY4NGJiOTQ5YmQ5ZGVkNiIsIm5iZiI6MTc3NzkxNTYwMS41MzIsInN1YiI6IjY5ZjhkNmQxY2ZlMTA5OTNlYmRmNTczYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BOdM0xjg3W246TZQTxQtxeTRmgXZD73klu19tQkiWF8'
+  }
 };
 
 
 // function for getting JSON data and returning it
 async function getData(url, opts) {
-    try {
-        const response = await fetch(url, opts);
-        if (response.ok) {
-            const result = await response.json();
-            return result;
-        } else {
-            throw(response.status);
-        }
-    } catch (error) {
-        console.error(error);
+  try {
+    const response = await fetch(url, opts);
+    if (response.ok) {
+      const result = await response.json();
+      return result;
+    } else {
+      throw (response.status);
     }
+  } catch (error) {
+    console.error(error);
+  }
 }
 document.addEventListener("DOMContentLoaded", function () {
 
-    
+
 });
 
 function fetchGenre(genreNumber) {
-    let genreSearchURL = 'https://api.themoviedb.org/3/discover/movie?with_genres=' + genreNumber;
-    getData(genreSearchURL, options).then(function(result) {
+  let genreSearchURL = 'https://api.themoviedb.org/3/discover/movie?with_genres=' + genreNumber;
+  getData(genreSearchURL, options).then(function (result) {
 
-        console.log("genre results:", result);
+    console.log("genre results:", result);
 
-        displayMovies(result);
-    });
+    displayMovies(result);
+  });
 }
 
-function displayMovies(movieData){
-    const results = document.getElementById("results");
-    results.innerHTML = ""; // Clear previous results
+function displayMovies(movieData) {
+  const results = document.getElementById("results");
+  results.innerHTML = ""; // Clear previous results
 
-    movieData.results.forEach(function(movie) {
-        console.log("movie:", movie);
-        results.innerHTML += `
+  movieData.results.forEach(function (movie) {
+    console.log("movie:", movie);
+    results.innerHTML += `
             <div class="movie-card">
             <h3>${movie.original_title}</h3>
             <p>Release Date: ${movie.release_date}</p>
             <p>Rating: ${movie.vote_average}</p>
             </div>
         `;
-    });
+  });
 }
 // had this before h3 tag <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.original_title} poster"></img>
 
@@ -133,8 +133,8 @@ function wheelOfFortune(selector) {
       iterations: 1
     });
 
-    
-    animation.onfinish = function() {
+
+    animation.onfinish = function () {
       const finalDegree = newEndDegree % 360;
       const sliceSize = 360 / genres.length;
 
@@ -142,21 +142,21 @@ function wheelOfFortune(selector) {
       const pointerOffset = 90;
 
       //const adjustedDegree = (360 - finalDegree + pointerOffset) % 360;
-      const adjustedDegree =  (360 - finalDegree + pointerOffset + sliceSize / 2) % 360;
-        selectedIndex = Math.floor(adjustedDegree / sliceSize);
+      const adjustedDegree = (360 - finalDegree + pointerOffset + sliceSize / 2) % 360;
+      selectedIndex = Math.floor(adjustedDegree / sliceSize);
 
       const selectedGenre = genres[selectedIndex];
 
       // show result
       // selectedGenreDiv.innerHTML = `<h2>You got: ${selectedGenre}</h2>`;
-        console.log("Spin result: ", selectedGenre);
+      console.log("Spin result: ", selectedGenre);
 
       // fetch movies
       const genreId = genreMap[selectedGenre];
       const url =
         "https://api.themoviedb.org/3/discover/movie?with_genres=" + genreId;
 
-      getData(url, options).then(function(result) {
+      getData(url, options).then(function (result) {
         displayMovies(result);
       });
     };
@@ -167,3 +167,5 @@ function wheelOfFortune(selector) {
 }
 
 wheelOfFortune('.ui-wheel-of-fortune');
+
+
